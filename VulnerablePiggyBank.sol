120963b927e291d98e9e5f326a02c533b57eb4ad
CeciliaOrlando <168267241+CeciliaOrlando@users.noreply.github.com>
2025-07-27 08:50:59 -0300

    Update VulnerablePiggyBank.sol

---
+++ VulnerablePiggyBank.sol
@@ -0,0 +1,10 @@
+// Identificador de licencia SPDX:
+pragma MIT solidity ^0.8.0;
+
+contrato VulnerablePiggyBank {
+    dirección pública propietario;
+    constructor() { propietario = msg.sender }
+    función deposit() público pagar {}
+    función retirar() público { pagar(msg.sender).transfer(dirección(this).balance); }
+    función ataque() público { }
+}
